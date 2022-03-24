5a18c5b7feabaff6622f14c540753ba4566dabd0
andrijapau <andrijapaurevic@gmail.com>
2022-03-24 14:45:00 -0400

    plots seem decent for q2 (c)

--- q2/q2_b.py
+++ q2/q2_b.py
@@ -40,13 +40,15 @@ def g_star(T):
         g_minus = lepton.e.g() + pion.pi_pm.g() + pion.pi_0.g() + X.g() + quark.top.g() + boson.W.g() + quark.bottom.g() + quark.charm.g() + quark.strange.g() + quark.up.g() + quark.down.g() + gluon.g() + lepton.tau.g()
         return g_star_max - g_minus
 
+    else:
+        return -1
 
-T_threshold = [100 * 1000, 30 * 1000, 15 * 1000, 1 * 1000, 200, 50, 25]  # MeV
-
-T = np.arange(25, 100 * 1000 + 1)[::-1]  # MeV
-g_star = [g_star(T_val) for T_val in T]
-
-for T_val in T_threshold:
-    plt.vlines(T_val, 0, 200, linestyles='dashed', colors='k')
-plt.loglog(T, g_star, 'k-')
-plt.show()
+# T_threshold = [100 * 1000, 30 * 1000, 15 * 1000, 1 * 1000, 200, 50, 25]  # MeV
+#
+# T = np.arange(25, 100 * 1000 + 1)[::-1]  # MeV
+# g_star = [g_star(T_val) for T_val in T]
+#
+# for T_val in T_threshold:
+#     plt.vlines(T_val, 0, 200, linestyles='dashed', colors='k')
+# plt.loglog(T, g_star, 'k-')
+# plt.show()
--- q2/q2_c.py
+++ q2/q2_c.py
@@ -1,16 +1,40 @@
-from q2_b import *
+from q2_b import g_star
+import numpy as np
+import matplotlib.pyplot as plt
 
-T_rh = 1e14 # GeV
+T_rh = 1e14  # GeV
+M_pl = 2.4e18
 
-T_a = lambda a: g_star(a) / a 
+T_a = lambda a: g_star(a) / a
 
-a_t = lambda t: sqrt(((pi/(3*M_pl)) * sqrt(g_star(t)/10) * g_star(t)**(-2/3))) * sqrt(t) 
+a_t = lambda t: np.sqrt(((np.pi / (3 * M_pl)) * np.sqrt(g_star(t) / 10) * g_star(t) ** (-2 / 3))) * np.sqrt(t)
 
-T_t = lambda t: g_star(a(t)) / a(t)
+T_t = lambda t: g_star(a_t(t)) / a_t(t)
 
-a_vals = np.arange(1e1, 1e9, 10) 
-#t_vals = np.arange(1e12, 1e26) # 1/GeV
+a_vals = np.arange(1, 1e6)
+t_vals = np.arange(1, 1e6)  # 1/GeV
 
+T_threshold = [100 * 1000, 30 * 1000, 15 * 1000, 1 * 1000, 200, 50, 25]  # MeV
+for T_val in T_threshold:
+    plt.vlines(T_val, 0, 200, linestyles='dashed', colors='k')
 
-plt.loglog(a_vals, T_a(a_vals))
+T_a_result = []
+for i in range(len(a_vals)):
+    T_a_result.append(T_a(a_vals[i]))
+
+a_t_result = []
+for i in range(len(t_vals)):
+    a_t_result.append(a_t(t_vals[i]))
+
+T_t_result = []
+for i in range(len(t_vals)):
+    T_t_result.append(T_t(t_vals[i]))
+
+plt.loglog(a_vals, T_a_result)
+plt.show()
+
+plt.loglog(a_vals, a_t_result)
+plt.show()
+
+plt.loglog(a_vals, T_t_result)
 plt.show()
