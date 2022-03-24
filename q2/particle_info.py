2ac56f3ffd41211d81ce25e31f051e268a31c22b
andrijapau <andrijapaurevic@gmail.com>
2022-03-23 21:49:17 -0400

    b done

--- q2/particle_info.py
+++ q2/particle_info.py
@@ -9,112 +9,155 @@ class quark:
 
     class top:
         def __init__(self):
-            pass
-        def g(self):
-            return 7/8 * 4 * 3
-        def critical_temp(self):
-            return 30 * 1000
+            self.leaves = True
 
+        def g(self):
+            return 7 / 8 * 4 * 3
+
+        def T(self):
+            return 30 * 1000
 
     class bottom:
         def __init__(self):
-            pass
-        def g(self):
-            return 7/8 * 4 * 3
-        def critical_temp(self):
-            return 1 * 1000 # 1 GeV
+            self.leaves = True
 
+        def g(self):
+            return 7 / 8 * 4 * 3
+
+        def T(self):
+            return 1 * 1000  # 1 GeV
 
     class charm:
         def __init__(self):
-            pass
-        def g(self):
-            return 7/8 * 4 * 3
-        def critical_temp(self):
-            return 200
+            self.leaves = True
 
+        def g(self):
+            return 7 / 8 * 4 * 3
+
+        def T(self):
+            return 200
 
     class strange:
         def __init__(self):
-            pass
-        def g(self):
-            return 7/8 * 4 * 3
-        def critical_temp(self):
-            return 200
+            self.leaves = True
 
+        def g(self):
+            return 7 / 8 * 4 * 3
+
+        def T(self):
+            return 200
 
     class up:
         def __init__(self):
-            pass
-        def g(self):
-            return 7/8 * 4 * 3
-        def critical_temp(self):
-            return 200
+            self.leaves = True
 
+        def g(self):
+            return 7 / 8 * 4 * 3
+
+        def T(self):
+            return 200
 
     class down:
         def __init__(self):
-            pass
+            self.leaves = True
+
         def g(self):
-            return 7/8 * 4 * 3
-        def critical_temp(self):
-            return 200 
+            return 7 / 8 * 4 * 3
+
+        def T(self):
+            return 200
+
 
 class gluon:
     def __init__(self):
-        pass
+        self.leaves = True
+
     def g(self):
         return 8 * 2
-    def critical_temp(self):
-        return 200 
 
-class W:
-    def __init__(self):
-        pass
-    def g(self):
-        return 3 * 2
-    def critical_temp(self):
-        return 15 * 1000
+    def T(self):
+        return 200
 
-class Z:
-    def __init__(self):
-        pass
-    def g(self):
-        return 3
-    def critical_temp(self):
-        return 15 * 1000 
 
-class h:
+class boson:
     def __init__(self):
-        pass
-    def g(self):
-        return 1
-    def critical_temp(self):
-        return 15 * 1000 
+        self.W = self.W()
+        self.Z = self.Z()
+        self.h = self.h()
+
+    class W:
+        def __init__(self):
+            self.leaves = True
+
+        def g(self):
+            return 3 * 2
+
+        def T(self):
+            return 15 * 1000
+
+    class Z:
+        def __init__(self):
+            self.leaves = True
+
+        def g(self):
+            return 3
+
+        def T(self):
+            return 15 * 1000
+
+    class h:
+        def __init__(self):
+            self.leaves = True
+
+        def g(self):
+            return 1
+
+        def T(self):
+            return 15 * 1000
+
 
 class X:
     def __init__(self):
-        pass
+        self.leaves = True
+
     def g(self):
         return 4
-    def critical_temp(self):
-        return 100 * 1000 
 
-class pi_pm:
-    def __init__(self):
-        pass
-    def g(self):
-        return 2
-    def critical_temp(self):
-        return 200 
+    def T(self):
+        return 100 * 1000
 
-class pi_0:
+
+class pion:
     def __init__(self):
-        pass
-    def g(self):
-        return 2
-    def critical_temp(self):
-        return 200 
+        self.pi_pm = self.pi_pm()
+        self.pi_0 = self.pi_0()
+
+    class pi_pm:
+        def __init__(self):
+            pass
+
+        def g(self):
+            return 2
+
+        def T_join(self):
+            return 200
+
+        def T_leave(self):
+            return 50
+
+    class pi_0:
+        def __init__(self):
+            pass
+
+        def g(self):
+            return 2
+
+        def T_join(self):
+            return 200
+
+        def T_leave(self):
+            return 50
+
 
 class lepton:
     def __init__(self):
@@ -124,51 +167,52 @@ class lepton:
 
     class e:
         def __init__(self):
-            pass
+            self.leaves = True
+
         def g(self):
-            return 7/8 * 4
-        def critical_temp(self):
-            return 250 / 1000 
+            return 7 / 8 * 4
+
+        def T(self):
+            return 250 / 1000
 
     class muon:
         def __init__(self):
-            pass
+            self.leaves = True
+
         def g(self):
-            return 7/8 * 4
-        def critical_temp(self):
-            return 50  
+            return 7 / 8 * 4
+
+        def T(self):
+            return 50
 
     class tau:
         def __init__(self):
-            pass
+            self.leaves = True
+
         def g(self):
-            return 7/8 * 4
-        def critical_temp(self):
+            return 7 / 8 * 4
+
+        def T(self):
             pass
+
 
 class photon:
     def __init__(self):
         pass
+
     def g(self):
         return 2
-    def critical_temp(self):
+
+    def T(self):
         pass
+
 
 class neutrino:
     def __init__(self):
         pass
-    def g(self):
-        return 7/8 * 2
-    def critical_temp(self):
-        pass
 
-quark = quark()
-gluon = gluon()
-W_boson = W()
-Z_boson = Z()
-h_boson = h()
-pi_pm = pi_pm()
-pi_0 = pi_0()
-lepton = lepton()
-photon = photon()
-neutrino = neutrino()
+    def g(self):
+        return 7 / 8 * 2
+
+    def T(self):
+        pass
---
+++ q2/q2_b.py
@@ -0,0 +1,52 @@
+from particle_info import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+quark = quark()
+gluon = gluon()
+boson = boson()
+pion = pion()
+lepton = lepton()
+X = X()
+
+
+def g_star(T):
+    g_star_max = 106.75
+    if T > X.T():
+        return g_star_max
+
+    if X.T() >= T > quark.top.T():
+        return g_star_max - X.g()
+
+    if quark.top.T() >= T > boson.W.T():
+        return g_star_max - (X.g() + quark.top.g())
+
+    if boson.W.T() >= T > quark.bottom.T():
+        return g_star_max - (X.g() + quark.top.g() + boson.W.g())
+
+    if quark.bottom.T() >= T > pion.pi_pm.T_join():
+        return g_star_max - (X.g() + quark.top.g() + boson.W.g() + quark.bottom.g())
+
+    if pion.pi_pm.T_join() >= T > pion.pi_pm.T_leave():
+        g_minus = X.g() + quark.top.g() + boson.W.g() + quark.bottom.g() + quark.charm.g() + quark.strange.g() + quark.up.g() + quark.down.g() + gluon.g() + lepton.tau.g()
+        g_plus = pion.pi_pm.g() + pion.pi_0.g()
+        return g_star_max - g_minus + g_plus
+
+    if pion.pi_pm.T_leave() >= T > lepton.e.T():
+        g_minus = pion.pi_pm.g() + pion.pi_0.g() + X.g() + quark.top.g() + boson.W.g() + quark.bottom.g() + quark.charm.g() + quark.strange.g() + quark.up.g() + quark.down.g() + gluon.g() + lepton.tau.g()
+        return g_star_max - g_minus
+
+    if lepton.e.T() >= T:
+        g_minus = lepton.e.g() + pion.pi_pm.g() + pion.pi_0.g() + X.g() + quark.top.g() + boson.W.g() + quark.bottom.g() + quark.charm.g() + quark.strange.g() + quark.up.g() + quark.down.g() + gluon.g() + lepton.tau.g()
+        return g_star_max - g_minus
+
+
+T_threshold = [100 * 1000, 30 * 1000, 15 * 1000, 1 * 1000, 200, 50, 25]  # MeV
+
+T = np.arange(25, 100 * 1000 + 1)[::-1]  # MeV
+g_star = [g_star(T_val) for T_val in T]
+
+for T_val in T_threshold:
+    plt.vlines(T_val, 0, 200, linestyles='dashed', colors='k')
+plt.loglog(T, g_star, 'k-')
+plt.show()
